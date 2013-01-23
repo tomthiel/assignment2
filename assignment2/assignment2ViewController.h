f7cb63f8a30eb928f73cfdc901e915d09b6cbec7
Tom Thiel <t_j_thiel@yahoo.com>
2013-01-23 14:10:51 -0600

    Assignment2 committed to GitHub

--- assignment2/assignment2ViewController.h
+++ assignment2/assignment2ViewController.h
@@ -9,5 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface assignment2ViewController : UIViewController
+//add outlets
+@property (weak, nonatomic) IBOutlet UISlider *ageSlider;
+@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *readySwitch;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
+@property (weak, nonatomic) IBOutlet UITextField *firstNameBox;
+//add actions
+- (IBAction)ageSliderChanged:(id)sender;
+- (IBAction)readySwitchChanged:(id)sender;
+- (IBAction)submitButtonClicked:(id)sender;
+- (IBAction)firstNameExit:(id)sender;
+- (IBAction)lastNameExit:(id)sender;
+
 
 @end
--- assignment2/assignment2ViewController.m
+++ assignment2/assignment2ViewController.m
@@ -14,10 +14,15 @@
 
 @implementation assignment2ViewController
 
+//need to synthesize outlets here too....
+@synthesize ageSlider,ageLabel, readySwitch,submitButton;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	    // Do any additional setup after loading the view, 
+    [submitButton setEnabled:NO ]; //set submit to disabled....
+    [readySwitch setOn:NO animated:YES]; //set switch to off...
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +31,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ageSliderChanged:(id)sender {
+    
+    float sliderValue = [ageSlider value];
+    int realSliderValue = (int) roundf(sliderValue);
+    
+    
+    NSLog(@"Quantity Slider Value = %f || int value = %i", sliderValue, realSliderValue);
+    
+    //create instance of NSString, initialize with format for integer
+    NSString *labelString = [[NSString alloc] initWithFormat:@"%i", realSliderValue  ];
+    //same result, simpler:
+  //  NSString *labelString_alt = [NSString stringWithFormat:@"i", realSliderValue];
+    
+    [ageLabel setText:labelString];
+
+}
+
+- (IBAction)readySwitchChanged:(id)sender {
+    // this should enable submit button
+    if ([readySwitch isOn]) {
+        [submitButton setEnabled:YES ];
+    }
+    else {
+        [submitButton setEnabled:NO];
+    }
+}
+
+- (IBAction)submitButtonClicked:(id)sender {
+         //display message when submit clicked
+        NSLog(@"SUCCESS Thanks for your submission!");
+    //create alert!
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Success!"
+                            
+        // send alert with name input
+         message:[NSString stringWithFormat:@"Thanks for your submission %@",_firstNameBox.text]
+                            
+       // message:@"Thanks for your submission."
+            delegate:nil
+            cancelButtonTitle:@"OK"
+            otherButtonTitles:nil];
+    [message show];
+   
+}
+
+- (IBAction)firstNameExit:(id)sender {
+}
+
+
+
+- (IBAction)lastNameExit:(id)sender {
+}
 @end
